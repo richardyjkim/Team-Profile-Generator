ea12690d227ef7f8c591d4670a6803d41aee88d0
Richard <richardyjkim@gmail.com>
2021-04-03 17:17:27 -0700

    Manager test passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -7,5 +7,25 @@ test("create an Manager object", () => {
   expect(manager.id).toBe('0001');
   expect(manager.email).toBe('hellodave@gmail.com');
   expect(manager.number).toBe('000-000-0001');
-  // expect(manager.getRole()).toBe('Manager');
+});
+
+test("creat a number object", () => {
+  const testNumber = "010-9920-6543"
+  const manager = new Manager('Dave', '0001', 'hellodave@gmail.com', testNumber);
+
+  expect(manager.number).toBe(testNumber);
+});
+
+test("if getRole() returns 'Manager'.", () => {
+  const role = "Manager";
+  const manager = new Manager('Dave', '0001', 'hellodave@gmail.com', '010-9920-6543');
+  
+  expect(manager.getRole()).toBe(role);
+});
+
+test("is getNumber() works", () => {
+  const testNumber = "010-9920-6543";
+  const manager = new Manager('Dave', '0001', 'hellodave@gmail.com', '010-9920-6543');
+
+  expect(manager.getNumber()).toBe(testNumber);
 });
